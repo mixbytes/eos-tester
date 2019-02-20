bad42daf35ecb605ef9f17106d8e75102013d301
Algys Ievlev <algys.ievlev@gmail.com>
2019-02-20 15:40:25 +0300

    /usr/bin -> /usr/local/bin

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ mkdir -p /opt/eos-tester
 cp eos-tester.sh /opt/eos-tester
 cp -r scripts /opt/eos-tester
 
-if [ ! -e /usr/bin/eos-tester ]; then
-    ln -s /opt/eos-tester/eos-tester.sh /usr/bin/eos-tester
+if [ ! -e /usr/local/bin/eos-tester ]; then
+    ln -s /opt/eos-tester/eos-tester.sh /usr/local/bin/eos-tester
 fi
